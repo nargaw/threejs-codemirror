94cf4ef51306f36c02861d4858c4e58104f6e98d
Nate <nateargaw@gmail.com>
2025-05-01 21:10:14 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,59 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls, plane } from 'three/examples/jsm/Addons.js'
+import * as fragmentShader from './Shaders/fragment.glsl'
+
+//canvas
+const canvas = document.querySelector('#webgl')
+
+//scene
+const scene = new THREE.Scene()
+
+//sizes
+let width = window.innerWidth
+let height = window.innerHeight
+
+//camera
+const fov = 45
+const aspect = width/height
+const camera = new THREE.PerspectiveCamera(fov, aspect, 1, 100)
+camera.position.set(0, 0, 3)
+
+//renderer
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(width, height)
+renderer.setClearColor(new THREE.Color('#1f1f1f'))
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 1))
+
+//camera controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.update()
+
+//shader plane
+const planeGeometry = new THREE.PlaneGeometry(1, 1, 10, 10)
+const planeMaterial = new THREE.ShaderMaterial()
+const shaderPlane = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(shaderPlane)
+
+//resize
+window.onresize = () =>{
+  width = window.innerWidth
+  height = window.innerHeight
+  renderer.setSize(width, height)
+  camera.aspect = width/height
+  camera.updateProjectionMatrix()
+}
+
+//time
+const clock = new THREE.Clock()
+
+//animate
+const animate = () => {
+  window.requestAnimationFrame(animate)
+  controls.update()
+  renderer.render(scene, camera)
+}
+
+animate()
+
